1687ad6e2db2f704f7d62266e23e0532f585996d
yeeberjeeber <aloysius.tan00@gmail.com>
2026-03-09 21:34:09 +0800

    Added heap comparison

--- RuntimeMemoryTracker.cpp
+++ RuntimeMemoryTracker.cpp
@@ -23,8 +23,19 @@ int main() {
 	cout << endl;
 
 	int stackVar = 5;
-	int* heapVar = new int(100);
 
+	int* heapVar = new int(100);
+	int* heapVar2 = new int(110);
+	int* heapVar3 = new int(120);
+	int* heapVar4 = new int(130);
+	int* heapVar5 = new int(140);
+
+	uintptr_t heapAddr = reinterpret_cast<uintptr_t>(heapVar);
+	uintptr_t heapAddr2 = reinterpret_cast<uintptr_t>(heapVar2);
+	uintptr_t heapAddr3 = reinterpret_cast<uintptr_t>(heapVar3);
+	uintptr_t heapAddr4 = reinterpret_cast<uintptr_t>(heapVar4);
+	uintptr_t heapAddr5 = reinterpret_cast<uintptr_t>(heapVar5);
+ 
 	static int initializedStatic = 10;
 	static int uninitializedStatic;
 
@@ -40,7 +51,20 @@ int main() {
 	cout << " stackVar: " << &stackVar << endl;
 	cout << endl;
 	cout << "Heap Segment " << endl;
-	cout << " heapVar: " << heapVar << endl;
+	cout << " heapVar address: " << heapVar << endl;
+	cout << " heapAddr decimal value: " << heapAddr << endl;
+	cout << endl;
+	cout << " heapVar2 address: " << heapVar2 << endl;
+	cout << " heapAddr2 decimal value: " << heapAddr2 << endl;
+	cout << endl;
+	cout << " heapVar3 address: " << heapVar3 << endl;
+	cout << " heapAddr3 decimal value: " << heapAddr3 << endl;
+	cout << endl;
+	cout << " heapVar4 address: " << heapVar4 << endl;
+	cout << " heapAddr4 decimal value: " << heapAddr4 << endl;
+	cout << endl;
+	cout << " heapVar5 address: " << heapVar5 << endl;
+	cout << " heapAddr5 decimal value: " << heapAddr5 << endl;
 	cout << endl;
 	cout << "Text Segment (function) " << endl;
 	cout << " sampleFunction: " << reinterpret_cast<void*>(&sampleFunction) << endl;
