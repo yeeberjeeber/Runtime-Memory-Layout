7b417198891658f7f7a31fd9be240a0110b93b43
yeeberjeeber <aloysius.tan00@gmail.com>
2026-03-09 21:11:20 +0800

    Added recursive function

--- RuntimeMemoryTracker.cpp
+++ RuntimeMemoryTracker.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdint>
 using namespace std;
 
 int initializedGlobal = 42;
@@ -6,29 +7,44 @@ int uninitializedGlobal;
 
 void sampleFunction() {}
 
+void recurse(int depth) {
+	int var;
+	uintptr_t varAddr = reinterpret_cast<uintptr_t>(&var);
+	cout << "Depth " << depth << " address: " << &var << endl;
+	cout << "Depth " << depth << " value: " << varAddr << endl;
+	cout << endl;
+	if (depth < 5) {
+		recurse(depth + 1);
+	}
+}
+
 int main() {
+	recurse(1);
+	cout << endl;
+
 	int stackVar = 5;
 	int* heapVar = new int(100);
 
 	static int initializedStatic = 10;
 	static int uninitializedStatic;
 
-	cout << "Initialized Data Segment: " << endl;
+	cout << "Initialized Data Segment " << endl;
 	cout << " initializedGlobal: " << &initializedGlobal << endl;
 	cout << " initializedStatic: " << &initializedStatic << endl;
-
-	cout << "BSS Segment (Uninitialized data): " << endl;
+	cout << endl;
+	cout << "BSS Segment (Uninitialized data) " << endl;
 	cout << " uninitializedGlobal: " << &uninitializedGlobal << endl;
 	cout << " uninitializedStatic: " << &uninitializedStatic << endl;
-
-	cout << "Stack Segment: " << endl;
+	cout << endl;
+	cout << "Stack Segment " << endl;
 	cout << " stackVar: " << &stackVar << endl;
-
-	cout << "Heap Segment: " << endl;
+	cout << endl;
+	cout << "Heap Segment " << endl;
 	cout << " heapVar: " << heapVar << endl;
-
-	cout << "Text Segment: " << endl;
+	cout << endl;
+	cout << "Text Segment (function) " << endl;
 	cout << " sampleFunction: " << reinterpret_cast<void*>(&sampleFunction) << endl;
+	cout << endl;
 
 	delete heapVar;
 	return 0;
